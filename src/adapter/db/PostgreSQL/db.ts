4c46cf35d79a4434db6e78124e037565a3d705f6
preamza02 <preamza02@gmail.com>
2024-11-03 13:05:39 +0700

    adding init db

--- src/adapter/db/PostgreSQL/db.ts
+++ src/adapter/db/PostgreSQL/db.ts
@@ -1,17 +1,26 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 export default function createPostgreSQLDBConnection(
-    user: string,
-    host: string,
-    database: string,
-    password: string,
-    port: number
+  user: string,
+  host: string,
+  database: string,
+  password: string,
+  port: number
 ): Pool {
-    return new Pool({
-        user: user,
-        host: host,
-        database: database,
-        password: password,
-        port: port,
-    });
+  return new Pool({
+    user: user,
+    host: host,
+    database: database,
+    password: password,
+    port: port,
+  });
+}
+
+export function initPostGreSQLDB(pool: Pool): void {
+  pool.query(
+    "CREATE TABLE IF NOT EXISTS payment (id SERIAL PRIMARY KEY, sender_id INT NOT NULL, receiver_id INT NOT NULL,amount INT NOT NULL,currency VARCHAR(3) NOT NULL, status VARCHAR(10) NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
+  );
+  pool.query(
+    "CREATE TABLE IF NOT EXISTS balance (id SERIAL PRIMARY KEY, userId INT NOT NULL, balance INT NOT NULL,currency VARCHAR(3) NOT NULL,  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
+  );
 }
--- src/server/core/cores.ts
+++ src/server/core/cores.ts
@@ -1,29 +1,32 @@
 import getConfig from "../../config/config";
 import Config from "../../config/config.type";
-import createPostgreSQLDBConnection from "../../adapter/db/PostgreSQL/db";
+import createPostgreSQLDBConnection, {
+  initPostGreSQLDB,
+} from "../../adapter/db/PostgreSQL/db";
 import InitAdapterCore from "./adapter";
 import initServiceCore from "./service";
 import InitPortCore from "./port";
 import PortCores from "./port.type";
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
-export default function InitCores():{
-    config:Config,
-    portCore:PortCores
+export default function InitCores(): {
+  config: Config;
+  portCore: PortCores;
 } {
-    const config:Config = getConfig();
-    const pool:Pool = createPostgreSQLDBConnection(
-        config.databaseConfig.user,
-        config.databaseConfig.host,
-        config.databaseConfig.database,
-        config.databaseConfig.password,
-        config.databaseConfig.port
-    );
-    const adapterCore = InitAdapterCore(pool);
-    const serviceCore = initServiceCore(adapterCore);
-    const portCore = InitPortCore(serviceCore);
-    return { 
-        config,
-        portCore 
-    };
+  const config: Config = getConfig();
+  const pool: Pool = createPostgreSQLDBConnection(
+    config.databaseConfig.user,
+    config.databaseConfig.host,
+    config.databaseConfig.database,
+    config.databaseConfig.password,
+    config.databaseConfig.port
+  );
+  initPostGreSQLDB(pool);
+  const adapterCore = InitAdapterCore(pool);
+  const serviceCore = initServiceCore(adapterCore);
+  const portCore = InitPortCore(serviceCore);
+  return {
+    config,
+    portCore,
+  };
 }
